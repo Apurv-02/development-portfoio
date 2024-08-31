10016971a8a44416877b8dc0b05bca4f00457617
Apurv <apurvkantaria1234@gmail.com>
2024-08-31 12:18:21 +0530

    Updates

---
+++ precache-manifest.3f6e6c524c61ea40f2541a8ad389d345.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5218af0be25d22e3a68614bcf5464189",
+    "url": "/development-portfoio/index.html"
+  },
+  {
+    "revision": "bb54ca1b36bfe0d68199",
+    "url": "/development-portfoio/static/css/main.0df89eae.chunk.css"
+  },
+  {
+    "revision": "902a6680a9fdd9d7fcc0",
+    "url": "/development-portfoio/static/js/2.94c36bd2.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/development-portfoio/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "bb54ca1b36bfe0d68199",
+    "url": "/development-portfoio/static/js/main.0fc17829.chunk.js"
+  },
+  {
+    "revision": "f111dde2f745bf293f96",
+    "url": "/development-portfoio/static/js/runtime~main.db7c6dc8.js"
+  },
+  {
+    "revision": "fb8c7370980216eba28364b693755fb5",
+    "url": "/development-portfoio/static/media/about_icon.fb8c7370.png"
+  },
+  {
+    "revision": "1ff23b7424d78e692366f2d565a862af",
+    "url": "/development-portfoio/static/media/home_page_logo.1ff23b74.png"
+  }
+]);
